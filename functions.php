3d6c108eb574ea1279f09e0a8175b81725e7205b
Sai <saicharan13@gmail.com>
2018-07-21 15:31:59 +0530

    Create notes post type

--- functions.php
+++ functions.php
@@ -4,5 +4,21 @@
 		wp_enqueue_style( 'notes-styles', get_theme_file_uri( '/style.css' ), NULL, '0.1' );
 	}
 	add_action( 'wp_enqueue_scripts', 'notes_files' );
-	
+
+	// Notes Post Type
+	function notes_post() {
+		register_post_type( 'note', array(
+			'supports' => array( 'title', 'editor' ),
+			'menu_icon' => 'dashicons-welcome-write-blog',
+			'show_ui' => true,
+			'labels' => array(
+				'name' => 'Notes',
+				'add_new_item' => 'Add Note',
+				'edit_item' => 'Edit Note',
+				'all_item' => 'All Notes',
+				'singular' => 'Note'
+			)
+		) );
+	}
+	add_action( 'init', 'notes_post' );
 ?>
