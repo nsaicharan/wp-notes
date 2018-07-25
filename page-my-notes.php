10e8bb4cf3ca234f70debd2dd08c4e26c2fd4965
Sai <saicharan13@gmail.com>
2018-07-25 23:58:06 +0530

    Add previous notes list

--- page-my-notes.php
+++ page-my-notes.php
@@ -10,6 +10,35 @@
 
         <button class="btn">Create Note</button>
     </form>
+
+    <br><br>
+    <hr>
+    <br><br>
+
+    <h3 class="headline headline--sm t-center">Previous Notes</h3>
+
+    <ul class="notes-list">
+        <?php 
+            $notesQuery = new WP_Query( array(
+                'post_type' => 'note',
+                'posts_per_page' => -1,
+                'author' => get_current_user_id()
+            ) );
+
+            while( $notesQuery->have_posts() ) : $notesQuery->the_post(); 
+        ?>
+
+        <li class="notes-list__item">
+            <a href="<?php the_permalink(); ?>" class="notes-list__title">
+                <?php echo get_the_title(); ?>
+            </a>
+
+            <p class="notes-list__content"><?php echo get_the_content(); ?></p>
+        </li>
+
+        <?php endwhile; ?>
+    </ul>
+
 </div>
 
 <?php get_footer(); ?>
